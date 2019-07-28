6a732b8387cd94be2d6be87d2b5834beac0f7d0f
muhamadnabila <mbilalghz.gmail.com>
2019-07-28 11:18:49 +0700

    testing 100% done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* istanbul ignore file */
+/* istanbul ignore else */
 if (process.env.NODE_ENV) {
    require('dotenv').config();
 }
@@ -7,10 +7,13 @@ const cors = require('cors')
 const route = require('./routes')
 const mongoose = require('mongoose')
 const app = express()
+const { errorHandling } = require ('./middlewares/errorHandling')
 const port = 3000
 
+/* istanbul ignore next */
 let database = process.env.ATLASS_PASS ? `mongodb+srv://root:${process.env.ATLASS_PASS}@cluster0-qtp0t.gcp.mongodb.net/namaDB?retryWrites=true&w=majority` : 'mongodb://localhost:27017/pindai' + process.env.NODE_ENV
 mongoose.connect(database, { useNewUrlParser: true }, function (err) {
+/* istanbul ignore next */   
    if (err) console.log('connection error')
    else console.log('mongoose is connected')
 })
@@ -22,31 +25,7 @@ app.use(express.json())
 app.use(route)
 
 app.use(function (err, req, res, next) {
-   if (err.name == 'ValidationError') {
-      let messages = []
-      for (key in err.errors) {
-         if (err.errors[key].reason) {
-            messages.push(err.errors[key].reason)
-         } else {
-            messages.push(err.errors[key].message)
-         }
-      }
-      res.status(404).json(messages)
-   }
-   else if (!err.code) {
-      if (err.message.includes('Cast to ObjectId failed')) {
-         res.status(404).json({ message: 'Bad request' })
-      } else {
-         res.status(500).json({ message: 'Internal server error' })
-      }
-   }
-   else {
-      if (err.name == 'MongoError') {
-         res.status(500).json({ message: err.errmsg })
-      } else {
-         res.status(err.code).json({ message: err.message })
-      }
-   }
+   return errorHandling(err,req,res,next)
 })
 
 app.listen(port, () => {

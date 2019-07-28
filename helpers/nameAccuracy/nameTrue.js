787dd2e161cb0f04470592096a4690cf7456bf0a
robbycp <robbycaesar@gmail.com>
2019-07-28 16:45:21 +0700

    extract name with accuracy

---
+++ helpers/nameAccuracy/nameTrue.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_130526.jpg': 'ROBBYCAESARPUTRA',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_141228.jpg': 'ROBBYCAESARPUTRA',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_173122.jpg': 'DEDISEPTIANS',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_153929.jpg': 'ROBBYNYUNKDIONJAN',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_153925.jpg': 'ROBBYNYUNKDIONJAN',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_153134.jpg': 'ROBBYNYUNKDIONJAN',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_153122.jpg': 'ROBBYNYUNKDIONJAN',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/IMG_20190727_173142.jpg': 'DEDISEPTIANS',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/ljkAdi.jpg': 'BUDI',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/ljk1.jpg': 'ROBBYNYUNKDIONJAN',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/ljkBudi.jpg': 'ADI',
+  'https://storage.googleapis.com/kantong-ajaib/testingPindai/ljktemplatesma.jpg': 'ROBBYCAESARPUTRA'
+}

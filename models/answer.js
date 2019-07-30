ea22e70ab09213b36e649e48ce19eeae8742820c
muhamadnabila <mbilalghz.gmail.com>
2019-07-30 20:20:01 +0700

    add hooks for count total score

--- models/answer.js
+++ models/answer.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const setSoal = require('../models/setSoal')
 
 let answerSchema = new mongoose.Schema({
   name: {
@@ -13,9 +14,42 @@ let answerSchema = new mongoose.Schema({
   setSoalId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'setSoal'
+  },
+  imageUrl : {
+    type: String,
   }
 },{ timestamps: true })
+
+answerSchema.pre('save', function (next) {
+  this.password = hash(this.password)
+  setSoal.findOne({_id: this.setSoalId})
+  .then(data => {
+    let score = 0
+    let totalNumber = Object.keys(data.answerKey).length
+    for (let key in data.answerKey) {
+      if(this.answers[key] == data.answerKey[key]) score += 100/totalNumber
+      else score -= 100/totalNumber
+    } 
+    this.score = score
+  })
+  next()
+})
 
 let Answer = mongoose.model('Answer', answerSchema)
 
 module.exports = Answer
+
+const answerKey = {
+  "1": "A",
+  "2": "C",
+  "3": "D",
+  "4": "E",
+  "5": "B"
+}
+const answers = {
+  "1": "A",
+  "2": "C",
+  "3": "D",
+  "4": "E",
+  "5": "B"
+}

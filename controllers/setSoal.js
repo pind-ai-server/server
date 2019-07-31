d9fcfb4f3aa880f907d14dbd3fc4482207ea8ada
rizky <andijani21.rizky@gmail.com>
2019-07-31 09:43:49 +0700

    logic csv

--- controllers/setSoal.js
+++ controllers/setSoal.js
@@ -1,5 +1,6 @@
 const Question = require('../models/setSoal')
 const User = require('../models/user')
+let converter = require('json-2-csv')
 class ControllerSetSoal {
     static create(req, res, next) {
         let input = { ...req.body }
@@ -19,7 +20,78 @@ class ControllerSetSoal {
     static findAll(req, res, next) {
         Question.find().populate('answers')
             .then(data => {
-                res.status(200).json(data)
+                let dataku = [{
+                    subjectName : "ppkn",
+                    avgScore : 60,
+                    passedStudent : 3,
+                    failedStudent : 22,
+                    passingGrade : 65,
+                    highestScore : 80,
+                    lowestScore : 55,
+                    questionSum : 30
+                },{
+                    subjectName : "penjaskes",
+                    avgScore : 60,
+                    passedStudent : 3,
+                    failedStudent : 22,
+                    passingGrade : 65,
+                    highestScore : 80,
+                    lowestScore : 55,
+                    questionSum : 30
+                },{
+                    subjectName : "matematika",
+                    avgScore : 60,
+                    passedStudent : 3,
+                    failedStudent : 22,
+                    passingGrade : 65,
+                    highestScore : 80,
+                    lowestScore : 55,
+                    questionSum : 30
+                },{
+                    subjectName : "bahasa indonesia",
+                    avgScore : 60,
+                    passedStudent : 3,
+                    failedStudent : 22,
+                    passingGrade : 65,
+                    highestScore : 80,
+                    lowestScore : 55,
+                    questionSum : 30
+                },{
+                    subjectName : "sejarah",
+                    avgScore : 60,
+                    passedStudent : 3,
+                    failedStudent : 22,
+                    passingGrade : 65,
+                    highestScore : 80,
+                    lowestScore : 55,
+                    questionSum : 30
+                }]
+                return converter.json2csvAsync(dataku,{delimiter : {wrap : false, field : ',', eol : '\n'},keys : ["subjectName","avgScore", "passedStudent", "failedStudent" , "passingGrade", "highestScore", "lowestScore", "questionSum"]} )
+                // return converter.json2csvAsync(data, {delimiter : {wrap : false, field : ',', eol : '\n'} ,keys : ["_id","UserId", "title", "folderName"]})
+                // var fields = Object.keys(data[0])
+                // var replacer = function(key, value) { return value === null ? '' : value } 
+                // var csv = data.map(function(row){
+                //     return fields.map(function(fieldName){
+                //       return JSON.stringify(row[fieldName], replacer)
+                //     }).join(',')
+                // })
+                // csv.unshift(fields.join(','))
+                // let a = csv.join('\r\n')
+                // let filename = "okeman"
+                // let uri = 'data:text/csv;charset=utf-8,' + escape(a)
+                // let link = document.createElement("a")
+                // link.download = filename.csv
+                // link.href = uri
+                // console.log(a);
+            })
+            .then(data=>{
+                console.log(data);
+                res.set({
+                    'Content-Type': 'text/csv; charset=UTF-8',
+                    'Content-Disposition': 'attachment; filename="file.csv"',
+                 });
+                // res.status(200).json(data)
+                res.send('\uFEFF' + data)
             })
             .catch(next)
     }
